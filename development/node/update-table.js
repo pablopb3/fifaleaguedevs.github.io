196229914d37fe913f5ad44023696e5bd4577266
Matheus Canhizares <sendto@matheuscanhizares.com>
2017-04-06 11:37:43 +0200

    Sort classification

--- development/node/update-table.js
+++ development/node/update-table.js
@@ -11,6 +11,7 @@ for(round in results){
 	addRoundResults(results[round]);
 }
 
+table = sortTable(table);
 fs.writeFileSync('../../_data/table.json', JSON.stringify(table, null, 2));
 
 
@@ -49,6 +50,54 @@ function addTeamInfo(teamName, points, goalFor,  goalAgainst){
         team.drawn++;
     }
 
+}
+
+function sortTable(table) {
+
+    var tableArray = tableToArray(table);
+    tableArray.sort(function(team1, team2) {
+        team1 = team1[1];
+        team2 = team2[1];
+        if(team1.points > team2.points){
+            return -1;
+        }
+
+        if(team2.points > team1.points){
+            return 1;
+        }
+
+        if(team1.goalDifference > team2.goalDifference){
+            return -1;
+        }
+
+        if(team2.goalDifference > team1.goalDifference){
+            return 1;
+        }
+
+        if(team1.goalAgainst > team2.goalAgainst){
+            return -1;
+        }
+
+        if(team2.goalAgainst > team1.goalAgainst){
+            return 1;
+        }
+
+        return 0;
+    });
+
+    return arrayToTable(tableArray);
+}
+
+function tableToArray(table) {
+    var tableArray = [];
+    for (var key in table) tableArray.push([key, table[key]]);
+    return tableArray;
+}
+
+function arrayToTable(tableArray) {
+    var table = {};
+    for(var key in tableArray) table[tableArray[key][0]] = tableArray[key][1];
+    return table;
 }
 
 function getEmptyData() {
